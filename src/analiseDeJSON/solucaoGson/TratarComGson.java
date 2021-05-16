e0d37f144ac0517d9ea687b8f19ec0d4be5c7d8c
Alcides Augusto Bezerra Neto <alcidesneto@consiste.com.br>
2021-05-16 15:58:50 -0300

    Desserializa Map generico com Gson

--- src/analiseDeJSON/solucaoGson/TratarComGson.java
+++ src/analiseDeJSON/solucaoGson/TratarComGson.java
@@ -1,6 +1,11 @@
 package analiseDeJSON.solucaoGson;
 
+import java.lang.reflect.Type;
+import java.util.Map;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 public class TratarComGson {
 	public static void main(String[] args) {
@@ -12,6 +17,18 @@ public class TratarComGson {
 		Pessoa p2 = gsonSupport.fromJson(p2JSON, Pessoa.class);
 		System.out.println(p2.getPrimeiroNome());
 		System.out.println(p2.getIdade());
+		
+		String json = "{\n"
+				+ "		\"linkorigem\": \"http://www.portaltransparencia.gov.br/download-de-dados/cepim\",\n"
+				+ "		\"origem\": \"PORTAL DA TRANSPARÊNCIA\"\n"
+				+ "	}";
+		
+		JsonObject obj = gsonSupport.fromJson(json, JsonObject.class);
+		System.out.println(obj.get("origem").getAsString());
+		
+		Type empMapType = new TypeToken<Map<String, Object>>() {}.getType();
+		Map<String, String> temp = gsonSupport.fromJson(json, empMapType);
+		System.out.println(gsonSupport.toJson(temp));
 		
 		
 	}
