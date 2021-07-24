8bdee0227e62422815d4ecbe78effd0b62654fc3
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-07-24 17:13:45 -0300

    Gets a folder path

--- src/fileSystem/FileSystemTool.java
+++ src/fileSystem/FileSystemTool.java
@@ -4,42 +4,59 @@ import javax.swing.JFileChooser;
 
 public class FileSystemTool {
 	private JFileChooser fileChooser = new JFileChooser();
+	private Integer selectionMode;
 	private Integer responseCod;
 	private Boolean done;
 
-	public String chooseAFile() {
+	public String chooseFile() {
 		String path;
-		
 		selectAFile();
-		
-		if (isDone()) {
-			path = fileChooser.getSelectedFile().toPath().toString();
-		} else {
-			path = null;
-		}
-		
+		path = isDone() ? fileChooser.getSelectedFile().toPath().toString() : null;
+		return path;
+	}
+
+	public String chooseDirectory() {
+		String path;
+		selectADirectory();
+		path = isDone() ? fileChooser.getSelectedFile().toPath().toString() : null;
 		return path;
 	}
 
 	private void selectAFile() {
+		setSelectionMode(JFileChooser.FILES_ONLY);
 		Integer codResponse = fileChooser.showOpenDialog(null);
 		setResponseCod(codResponse);
-	}	
+	}
+
+	private void selectADirectory() {
+		setSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		Integer codResponse = fileChooser.showOpenDialog(null);
+		setResponseCod(codResponse);
+	}
 
 	private void setResponseCod(Integer responseCod) {
 		this.responseCod = responseCod;
 		setDone();
 	}
-	
+
 	public Integer getResponseCod() {
 		return responseCod;
 	}
-	
+
 	private boolean setDone() {
 		return done = responseCod == JFileChooser.APPROVE_OPTION;
 	}
-	
+
 	public boolean isDone() {
 		return done;
+	}
+
+	private void setSelectionMode(Integer selectionMode) {
+		this.selectionMode = selectionMode;
+		fileChooser.setFileSelectionMode(selectionMode);
+	}
+
+	public Integer getSelectionMode() {
+		return selectionMode;
 	}
 }
--- src/fileSystem/FyleSystemApp.java
+++ src/fileSystem/FyleSystemApp.java
@@ -2,8 +2,9 @@ package fileSystem;
 
 public class FyleSystemApp {
 	public static void main(String[] args) {
-		FileSystemTool fileSystemTool = new FileSystemTool();		
-		System.out.println(fileSystemTool.chooseAFile());
+		FileSystemTool fileSystemTool = new FileSystemTool();
+		System.out.println(fileSystemTool.chooseFile());
+		System.out.println(fileSystemTool.chooseDirectory());
 
 	}
 
